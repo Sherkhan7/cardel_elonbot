884d412567a0fef44f8c9f4eabadef67cea1e5af
sherkhan7 <9669470@gmail.com>
2020-11-19 02:12:57 +0500

    minor ch: add/update variables

--- globalvariables/globalvariables.py
+++ globalvariables/globalvariables.py
@@ -1,8 +1,8 @@
 USER_ID, TG_ID, USER_TG_ID, NAME, SURNAME, USERNAME, FROM_REGION, FROM_DISTRICT, FROM_LOCATION = \
     ('user_id', 'tg_id', 'user_tg_id', 'name', 'surname', 'username', 'from_region', 'from_district', 'from_location')
 
-TO_REGION, TO_DISTRICT, TO_LOCATION, WEIGHT_UNIT, WEIGHT, VOLUME_UNIT = \
-    ('to_region', 'to_district', 'to_location', 'weight_unit', 'weight', 'volume_unit')
+TO_REGION, TO_DISTRICT, TO_LOCATION, WEIGHT_UNIT, WEIGHT, VOLUME_UNIT, USER_INPUT_DATA = \
+    ('to_region', 'to_district', 'to_location', 'weight_unit', 'weight', 'volume_unit', 'user_input_data')
 
 VOLUME, DEFINITION, PHOTO, DATE, TIME, USER_PHONE_NUMBER, CONFIRMATION, EDIT, MESSAGE_ID = \
     ('volume', 'definition', 'photo', 'date', 'time', 'user_phone_number', 'confirmation', 'edit', 'message_id')
@@ -10,8 +10,8 @@ VOLUME, DEFINITION, PHOTO, DATE, TIME, USER_PHONE_NUMBER, CONFIRMATION, EDIT, ME
 FROM_TEXT, TO_TEXT, WEIGHT_TEXT, VOLUME_TEXT, DEFINITION_TEXT, DATE_TEXT, POST_ID = \
     ('from_text', 'to_text', 'weight_text', 'volume_text', 'definition_text', 'date_text', 'post_id')
 
-TIME_TEXT, CLIENT_TEXT, CLIENT_PHONE_NUMBER_TEXT, STATE, REGION_NAME, ID, LANG = \
-    ('time_text', 'client_text', 'client_phone_number_text', 'state', 'region_name', 'id', 'lang')
+TIME_TEXT, USER_TEXT, USER_PHONE_NUMBER_TEXT, STATE, REGION_NAME, ID, LANG = \
+    ('time_text', 'user_text', 'user_phone_number_text', 'state', 'region_name', 'id', 'lang')
 
 STATUS_TEXT, OPENED_STATUS, CLOSED_STATUS, NOT_CONFIRMED_STATUS, TG_ACCOUNT_TEXT, UNDEFINED_TEXT = \
     ('status_text', 'opened_status', 'closed_status', 'not_confirmed_status', 'tg_account_text', 'undifened_text')
